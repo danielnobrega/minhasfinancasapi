3873da13d3177d28f7b7547af63263628e3ffdf3
Daniel Aguiar da Nobrega <daniel.nobrega@seduc.ce.gov.br>
2020-06-24 11:47:29 -0300

    Concluído o Controller de Lançamento

--- src/main/java/com/danielnobrega/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/danielnobrega/minhasfinancas/model/repository/LancamentoRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.danielnobrega.minhasfinancas.enums.TipoLancamento;
 import com.danielnobrega.minhasfinancas.model.entity.Lancamento;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
 	
 	@Query( value = " select sum(l.valor) from Lancamento l join l.usuario u "
-				  + " where u.id = :idUsuario and l.tipo = = :tipo group by u")
-	BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") String tipo);
+				  + " where u.id = :idUsuario and l.tipo = :tipo group by u")
+	BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo);
 
 }
